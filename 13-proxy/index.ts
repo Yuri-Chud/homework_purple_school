a764c4ae64adf4d582e7aa6c225c2a48207509aa
Yuri-Chud <yurij.chudinov.99@mail.ru>
2023-11-19 20:38:16 +0300

    13-proxy v1.0

---
+++ 13-proxy/index.ts
@@ -0,0 +1,34 @@
+/*
+написать Proxy для класса API, котоая будет отправлять запросы на 
+ 'https://dummyjson.com/products/1'если id продукта < 10, иначе 
+ вернуть ошибку
+ */
+
+class DummyAPI implements Record <string, any> {
+	getDummyJSON(id: number): Record <string, any> | undefined {
+    const result : Record <string, any> = fetch(`https://dummyjson.com/products/${id}`)
+      .then(res => res.json())
+      .then(json => console.log(json));
+		return result;
+	}
+}
+
+class DummyAccessProxy implements Record <string, any> {
+	constructor(private api: DummyAPI) { }
+
+	getJSONById(id: number): Record <string, any> | undefined {
+		if (id <= 10) {
+			return this.api.getDummyJSON(id)
+		}
+    else{
+      console.log('id должен быть не больше 10!');
+      return undefined;
+    }
+	}
+}
+
+const proxy = new DummyAccessProxy(new DummyAPI());
+console.log(proxy.getJSONById(1));
+
+const proxy2 = new DummyAccessProxy(new DummyAPI());
+console.log(proxy2.getJSONById(20));
