6d4c3413b506ccb4a228271b2cf0ef22b96cb6bc
Yuri-Chud <yurij.chudinov.99@mail.ru>
2023-11-19 14:42:35 +0300

    9-difference v.2

--- 9-difference/index.js
+++ 9-difference/index.js
@@ -3,15 +3,14 @@
 функция, которая удалит все ключи из первого объекта,
 которые есть во втором объекте.
 */
-let a = { a: 5, b: '' };
-let b = { a: 10, c: true };
+let aObj = { a: 5, b: '', e: '' };
+let bObj = { c: true, b: '' };
 function differenceInObj(AObj, BObj) {
     const keysA = Object.keys(AObj);
     const keysB = Object.keys(BObj);
     const diffKeys = keysA.filter(item => !keysB.includes(item));
     const resObject = diffKeys.reduce((o, k) => { o[k] = AObj[k]; return o; }, {});
-    const resObjWithTypes = resObject; // (в этой строчке вроде смысла нету) получаем типизированный обьект,  только с теми типами из T1, которые есть в resTypes
-    return resObjWithTypes;
+    return resObject;
 }
-let v0 = differenceInObj(a, b);
+let v0 = differenceInObj(aObj, bObj);
 console.log(v0);
--- 9-difference/index.ts
+++ 9-difference/index.ts
@@ -6,32 +6,31 @@
 interface IA {
     a: number;
     b: string;
+    e: string;
 }
 
 
 interface IB {
-    a: number;
+
     c: boolean;
+    b: string;
 }
 
 
-let a: IA = { a: 5, b: ''};
-let b: IB = { a: 10, c: true};
+let aObj: IA = { a: 5, b: '', e: ''};
+let bObj: IB = { c: true, b: ''};
 
 interface IDifference {
     b: string;
 }
 
-function differenceInObj <T1 extends {}, T2 extends {} >(AObj:T1, BObj:T2) {
+function differenceInObj <T1 extends {}, T2 extends {} >(AObj:T1, BObj:T2) : Omit<T1,  keyof T1 &  keyof T2> {
     const keysA = Object.keys(AObj);
     const keysB = Object.keys(BObj);
     const diffKeys = keysA.filter(item => !keysB.includes(item));
-    const resObject = diffKeys.reduce((o,k) => {o[k] = AObj[k]; return o},
-        {});
-    type resTypes = Pick<T1, keyof typeof resObject>; // типы полученного обьекта
-    const resObjWithTypes: resTypes = resObject // (в этой строчке вроде смысла нету) получаем типизированный обьект,  только с теми типами из T1, которые есть в resTypes
-    return resObjWithTypes;
+    const resObject : any = diffKeys.reduce((o,k) => {o[k] = AObj[k]; return o}, {});
+    return resObject
     }
 
-let v0 = differenceInObj(a, b);
+let v0 = differenceInObj(aObj, bObj);
 console.log(v0);
